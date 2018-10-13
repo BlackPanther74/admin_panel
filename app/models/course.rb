86d9f257349b41d8337d1e4b2c915b03d7525e7a
Garth Puckerin <garth.puckerin@gmail.com>
2018-10-13 00:28:46 -0400

    Added validation to the course and student controllers and models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
   has_many :cohorts
+
+  validates_presence_of :name
+  validates :hours, numericality: {only_interger: true, greater_than: 0,  message: "Courses must be at least 1 hour long!"}
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
   has_many :cohortsstudents
   has_many :cohorts, through: :cohortsstudents
+
+  validates_presence_of :first_name, :last_name, :age, :education
+  validates :age, numericality: {only_interger: true, greater_than: 0, less_than: 150,  message: "Please enter age!"}
 end
