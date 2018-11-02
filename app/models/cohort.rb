f6dfdaad4ee3cbfde6299666e8b77015f5c2418e
Garth Puckerin <garth.puckerin@gmail.com>
2018-11-01 22:08:51 -0400

    Minor corrections

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,7 +1,7 @@
 class Cohort < ApplicationRecord
   belongs_to :course
   has_one :instructor
-  has_many :cohortsstudents
+  has_many :cohortsstudents, dependent: :destroy
   has_many :students, through: :cohortsstudents
 
   validates_presence_of :name, :start_date, :end_date
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,6 +2,6 @@ class Instructor < ApplicationRecord
   belongs_to :cohort, {optional: true}
 
   validates_presence_of :first_name, :last_name, :age, :education, :salary
-  validates :age, numericality: {only_integer: true, greater_than: 0, less_than: 150,  message: "Please enter a valid age! (< 151)"}
-  validates :salary, numericality: {only_integer: true, greater_than: 0, message: "Please enter an amount greater than $0"}
+  validates :age, numericality: {only_integer: true, greater_than: 0, less_than: 150,  alert: "Please enter a valid age! (< 151)"}
+  validates :salary, numericality: {only_integer: true, greater_than: 0, alert: "Please enter an amount greater than $0"}
 end
